1329585efba7926d4a5fff289d2642fb2b2ad104
tknott95 <tknott95@hotmail.com>
2016-12-18 13:45:05 -0700

    NALS: Register Auth

--- routes/users.js
+++ routes/users.js
@@ -17,8 +17,38 @@ router.get('/login', function(req, res, next) {
   res.render('login', {title: 'Login'});
 });
 
-router.post('/register', upload.single('profile-image'), function(req, res, next) {
-  console.log(req.body.name);
+router.post('/register', upload.single('profileimage'), function(req, res, next) {
+  var name = req.body.name;
+  var email = req.body.email;
+  var username = req.body.username;
+  var password = req.body.password;
+  var password2 = req.body.password2;
+
+//   req.file ? console.log('Uploading File...') : console.log('No File Uploaded.');
+
+  if (req.file) {
+    console.log('Uploading File...');
+    var profileimage = req.file.filename;
+  } else {
+    console.log('No File Uploaded.');
+    var profileimage = 'noimage.jpg';
+  }
+
+  // Form Validator
+  req.checkBody('name', 'Name field is required').notEmpty();
+  req.checkBody('email', 'Email field is required').notEmpty();
+  req.checkBody('email', 'Email is not valid').isEmail();
+  req.checkBody('username', 'Username field is required').notEmpty();
+  req.checkBody('password', 'Password field is required').notEmpty();
+  req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+
+
+  // Check Errors
+  var errors = req.validationErrors();
+
+  errors ? res.render('register', {
+    errors: errors
+  }) : console.log('No Errors');
 });
 
 module.exports = router;
